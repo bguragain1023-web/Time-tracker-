d5722cf270dbaa829bba269cb3d5524a8920bce5
Brazesh Guragain <b.guragain1023@gmail.com>
2026-03-10 21:53:19 +1100

    project completed

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-let entryList = [];
+let taskList = [];
+
+const hourPerWeek = 168;
 
 const handleOnSubmit = (e) => {
   const newForm = new FormData(e);
 
   const task = newForm.get("task");
-  const hour = newForm.get("hr");
+  const hour = +newForm.get("hr");
 
   const entryData = {
     task: task,
     hour: hour,
     id: randomId(),
+    type: "entry",
   };
 
-  entryList.push(entryData);
+  const ttlAllocatedHrs = ttlHours();
+
+  if (ttlAllocatedHrs + hour > hourPerWeek) {
+    return alert("Number of hour per weeek exceeded");
+  }
+
+  taskList.push(entryData);
   displayEntrylist();
 };
 
 const displayEntrylist = () => {
   const entryElm = document.getElementById("entryList");
+
+  const entryList = taskList.filter((item) => item.type == "entry");
   let str = "";
   entryList.map((item, i) => {
     str += `
@@ -31,7 +42,9 @@ const displayEntrylist = () => {
                   }')"   class="btn btn-danger">
                     <i class="fa-solid fa-trash"></i>
                   </button>
-                  <button class="btn btn-success">
+                  <button  onclick ="switchTask('${
+                    item.id
+                  }', 'bad')" class="btn btn-success">
                     <i class="fa-solid fa-arrow-right"></i>
                   </button>
                 </td>
@@ -39,10 +52,49 @@ const displayEntrylist = () => {
   `;
   });
   entryElm.innerHTML = str;
+
+  ttlHours();
+};
+
+const displayBadlist = () => {
+  //   const badHrsElm = document.getElementById("badhours");
+  const badElm = document.getElementById("badList");
+  const badList = taskList.filter((item) => item.type === "bad");
+  let str = "";
+  badList.map((item, i) => {
+    str += `
+  <tr>
+                <td>${i + 1}</td>
+                <td>${item.task}</td>
+                <td>${item.hour}hr</td>
+                <td class="text-end">
+                 
+                  <button  onclick ="switchTask('${
+                    item.id
+                  }', 'entry')" class="btn btn-warning">
+                    <i class="fa-solid fa-arrow-left"></i>
+                  </button>
+
+                   <button  onclick = "deleteItem('${
+                     item.id
+                   }')"   class="btn btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+
+                </td>
+              </tr>
+  `;
+  });
+  badElm.innerHTML = str;
+
+  document.getElementById("savedHour").innerHTML = badList.reduce(
+    (acc, item) => acc + item.hour,
+    0,
+  );
 };
 
 const randomId = (length = 6) => {
-  console.log(entryList);
+  console.log(taskList);
 
   const str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
   let id = "";
@@ -56,7 +108,33 @@ const randomId = (length = 6) => {
 
 const deleteItem = (id) => {
   if (window.confirm("Are you sure yo want to delete this entry ?")) {
-    entryList = entryList.filter((item) => item.id !== id);
+    taskList = taskList.filter((item) => item.id !== id);
     displayEntrylist();
+    displayBadlist();
   }
 };
+
+const switchTask = (id, type) => {
+  taskList = taskList.map((item) => {
+    if (item.id === id) {
+      item.type = type;
+    }
+    return item;
+  });
+  displayEntrylist();
+  displayBadlist();
+};
+
+const ttlHours = () => {
+  const totalHours = taskList.reduce((acc, item) => {
+    return acc + item.hour;
+  }, 0);
+
+  document.getElementById("ttlHours").innerHTML = totalHours;
+
+  return totalHours;
+};
+
+// const savedHours = () => {
+
+// };
